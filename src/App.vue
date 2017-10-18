b3dad2284648189e572b6f2819e4f98a17a3d987
mawglih <mawglih@gmail.com>
2017-10-18 00:21:53 -0400

    'initial'

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <h1 class="text-center">{{msg}}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'Generate your starbase team'
+    }
+  }
+}
+</script>
+
+<style>
+  body {
+    background-color: #263238;
+    color: bisque;
+  }
+  #app {
+     font-family: 'Avenir', Arial, Helvetica, sans-serif;
+     text-align:center;
+     margin-top:60px;
+  }
+</style>
